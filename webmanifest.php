7b5fc01dcc69ec28a50d2b4b937e95d99de87dfd
Royal12322 <royalcity2025@gmail.com>
2024-12-13 09:37:46 +0600

    Add files via upload

---
+++ webmanifest.php
@@ -0,0 +1,10 @@
+{
+    "name": "XtremePapers",
+    "description": "Ask questions and exchange answers on past papers, revision, exams, student life or whatever is on your mind on the greatest educational forums.",
+    "lang": "en-US",
+    "dir": "LTR",
+    "display": "minimal-ui",
+    "scope": "\/",
+    "start_url": "\/?_pwa=1",
+    "background_color": "#080808"
+}
